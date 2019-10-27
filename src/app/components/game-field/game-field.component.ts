292750e41a160433302540748c30637dd648f5ef
ikozikov <ivan.kozikov@nixsolutions.com>
2019-10-27 17:25:25 +0200

    Game field create grid and parameters

--- src/app/components/game-field/game-field.component.ts
+++ src/app/components/game-field/game-field.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ModeInterface} from '../../models/game-settings.interface';
 
 @Component({
   selector: 'app-game-field',
   templateUrl: './game-field.component.html',
   styleUrls: ['./game-field.component.scss']
 })
-export class GameFieldComponent implements OnInit {
+export class GameFieldComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() gameMode: ModeInterface;
+
+  gameField: any[];
+  private notUsedSquares: any[];
+
+  constructor() {
+    this.gameField = [];
+    this.notUsedSquares = [];
+  }
 
   ngOnInit() {
+    //Todo remove
+    this.gameMode = {delay: 2000, field: 10};
+
+    this.createGameField();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.gameMode && !changes.gameMode.isFirstChange()) {
+      this.clearGameField();
+      this.createGameField();
+    }
+  }
+
+  get gameDelay(): number {
+    return this.gameMode.delay;
+  }
+
+  get fieldSize(): number {
+    return this.gameMode.field;
+  }
+
+  private createGameField(): void {
+    for (let i = 0; i < Math.pow(this.fieldSize, 2); i++) {
+      this.createSquare(i);
+    }
+  }
+
+  private createSquare(id: number): void {
+    const squareId = `sqr${id}`;
+    this.gameField.push({squareId});
+  }
+
+  get gridTemplate(): string {
+    return `repeat(${this.fieldSize}, minmax(50px, 1fr))`;
+  }
+
+  private clearGameField(): void {
+    this.gameField = [];
+    this.notUsedSquares = [];
   }
 
 }
